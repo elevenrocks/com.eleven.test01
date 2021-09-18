e0956039857bd563601ed89431e571b4de293edb
eleven <1055778391@qq.com>
2021-09-19 02:13:41 +0800

    1.3

--- TestGitHub/src/com/eleven/git/Person.java
+++ TestGitHub/src/com/eleven/git/Person.java
@@ -4,4 +4,15 @@ public class Person {
     private Integer id;
     private String name;
     private String address;
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(Integer id, String name, String address, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
 }
