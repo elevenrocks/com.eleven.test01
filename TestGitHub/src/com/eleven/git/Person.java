4c9149d5ee8738188f2136570b25ef24f6355cde
eleven <1055778391@qq.com>
2021-09-19 02:22:20 +0800

    1.5

--- TestGitHub/src/com/eleven/git/Person.java
+++ TestGitHub/src/com/eleven/git/Person.java
@@ -15,4 +15,36 @@ public class Person {
         this.address = address;
         this.age = age;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
